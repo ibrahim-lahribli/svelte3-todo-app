631b759371d319bc6d310c67f235fe0eedef0048
Ibrahim Lahribli <dt.lahribli.ibrahim@gmail.com>
2019-12-26 14:55:19 +0100

    add template code for todos components

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -1,4 +1,9 @@
 <script>
+  import TodoItem from "./TodoItem";
+  let newTodoTitle = "";
+  let currentFilter = "all";
+  let nextId = 4;
+
   let todos = [
     {
       id: 1,
@@ -17,3 +22,60 @@
     }
   ];
 </script>
+
+<div class="container">
+  <a href="https://google.com" target="blank">
+    <img src={'/img/test.png'} alt="svelte logo" class="logo" />
+  </a>
+
+  <h2>Svelte todo App</h2>
+  <input
+    type="text"
+    class="todo-input"
+    placeholder="insert todo item ..."
+    bind:value={newTodoTitle}
+    on:keydown={addTodo} />
+  {#each filteredTodos as todo}
+    <div class="todo-item">
+      <TodoItem
+        {...todo}
+        on:deleteTodo={handleDeleteTodo}
+        on:toggleComplete={handleToggleComplete} />
+    </div>
+  {/each}
+  <div class="inner-container">
+    <div>
+      <label>
+        <input
+          class="inner-container-input"
+          type="checkbox"
+          on:change={checkAllTodos} />
+        Check All
+      </label>
+    </div>
+    <div>{todosRemaining} items left</div>
+  </div>
+
+  <div class="inner-container">
+    <div>
+      <button
+        on:click={() => updateFilter('all')}
+        class:active={currentFilter === 'all'}>
+        All
+      </button>
+      <button
+        on:click={() => updateFilter('active')}
+        class:active={currentFilter === 'active'}>
+        Active
+      </button>
+      <button
+        on:click={() => updateFilter('completed')}
+        class:active={currentFilter === 'completed'}>
+        Completed
+      </button>
+
+      <button on:click={clearCompleted}>Clear Completed</button>
+
+    </div>
+  </div>
+</div>
